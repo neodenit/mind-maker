1400218d87f9f36af1827106e3ed39a6a9cc3574
Denis Nekrylov <neodenit@gmail.com>
2021-10-29 22:53:12 +0300

    Add string conversion, logging and exception handling

--- Neodenit.MindMaker.Services.GPT3/GetAdvice.cs
+++ Neodenit.MindMaker.Services.GPT3/GetAdvice.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
@@ -28,29 +29,73 @@ namespace Neodenit.MindMaker.Services.GPT3
             var logger = executionContext.GetLogger(nameof(GetAdvice));
             logger.LogInformation(nameof(GetAdvice));
 
-            var request = await JsonSerializer.DeserializeAsync<AdviceRequestDTO>(req.Body);
+            try
+            {
+                var request = await JsonSerializer.DeserializeAsync<AdviceRequestDTO>(req.Body);
 
-            IEnumerable<IEnumerable<string>> branches = GetBranches(request.Root);
-            var textBranches = branches.Select(b => string.Join(settings.NodeSeparator, b));
-            var context = string.Join(settings.BlockSeparator, textBranches);
-            var prompt = string.Join(settings.NodeSeparator, request.Parents);
+                var nodeSeparator = FixString(settings.NodeSeparator);
+                var blockSeparator = FixString(settings.BlockSeparator);
 
-            var fullPrompt = context + settings.BlockSeparator + prompt + settings.NodeSeparator;
+                IEnumerable<IEnumerable<string>> branches = GetBranches(request.Root);
+                var textBranches = branches.Select(b => string.Join(nodeSeparator, b));
+                var context = string.Join(settings.BlockSeparator, textBranches);
+                var prompt = string.Join(nodeSeparator, request.Parents);
 
-            OpenAIAPI api = new(APIAuthentication.LoadFromEnv(), new Engine(settings.Engine));
+                var fullPrompt = context + settings.BlockSeparator + prompt + nodeSeparator;
 
-            CompletionResult completionResult = await api.Completions.CreateCompletionAsync(fullPrompt, settings.MaxTokens, settings.Temperature, numOutputs: settings.NumOutputs, stopSequences: settings.StopSequences);
+                var stopSequences = settings.StopSequences.Select(s => FixString(s)).ToArray();
 
-            var resuts = completionResult.Completions.Select(c => c.Text);
+                OpenAIAPI api = new(APIAuthentication.LoadFromEnv(), new Engine(settings.Engine));
 
-            var response = req.CreateResponse();
+                CompletionResult completionResult = await api.Completions.CreateCompletionAsync(fullPrompt, settings.MaxTokens, settings.Temperature, numOutputs: settings.NumOutputs, stopSequences: stopSequences);
 
-            await response.WriteAsJsonAsync(resuts);
-            return response;
+                var resuts = completionResult.Completions.Select(c => c.Text.Trim()).Distinct();
+
+                LogRequest(logger, fullPrompt, resuts, stopSequences);
+
+                var response = req.CreateResponse();
+                await response.WriteAsJsonAsync(resuts);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
         }
+
+        private readonly Dictionary<string, string> Replacements = new()
+        {
+            { "\\n", "\n" },
+            { "\\s", " " }
+        };
+
+        private string FixString(string s) =>
+            Replacements.Aggregate(s, (acc, item) => acc.Replace(item.Key, item.Value));
 
         private IEnumerable<IEnumerable<string>> GetBranches(NodeDTO node) =>
             GetBranches(node, Enumerable.Empty<string>());
+
+        private void LogRequest(ILogger logger, string request, IEnumerable<string> response, IEnumerable<string> stopSequences)
+        {
+            logger.LogInformation(
+                JsonSerializer.Serialize(
+                    new
+                    {
+                        Function = nameof(GetAdvice),
+                        settings.Engine,
+                        settings.Temperature,
+                        settings.MaxTokens,
+                        StopSequences = stopSequences,
+                        Request = request,
+                        Response = response
+                    },
+                    new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                    }));
+        }
 
         private IEnumerable<IEnumerable<string>> GetBranches(NodeDTO node, IEnumerable<string> acc)
         {
