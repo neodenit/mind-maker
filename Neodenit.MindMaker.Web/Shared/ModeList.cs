50a65102f8e94de257e1734dc884cd466ec28f90
Denis Nekrylov <neodenit@gmail.com>
2021-11-26 22:48:19 +0200

    Add mode labels

---
+++ Neodenit.MindMaker.Web/Shared/ModeList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Neodenit.MindMaker.Web.Shared
+{
+    public static class ModeList
+    {
+        public static IEnumerable<(Mode mode, string label)> Modes { get; set; } = new[]
+        {
+            (Mode.Branch, "Branches"),
+            (Mode.SubBranch, "Sub-Branches"),
+            (Mode.SimpleBranch, "Simple Branches"),
+            (Mode.SimpleSubBranch, "Simple Sub-Branches")
+        };
+    }
+}
